b6c85cc2801201fa648bbf85d7d4db5e86518cf7
ardaalkan <ardaaa.alk@gmail.com>
2023-07-31 00:54:38 +0300

    added cursor component the global

--- src/components/InfinityImage/InfinityImage.jsx
+++ src/components/InfinityImage/InfinityImage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./InfinityImage.css";
+import car1 from "../../assets/f1-1.jpg";
+import car2 from "../../assets/f1-2.jpg";
+import car3 from "../../assets/f1-3.jpg";
+import car4 from "../../assets/f1-4.jpg";
+import car5 from "../../assets/f1-5.jpg";
 
 const InfinityImage = () => {
-  return <div>InfinityImage</div>;
+  return (
+    <div className="image-container">
+      <div className="slider">
+        <div>
+          <img src={car1} alt="f1 car" className="image-container_image" />
+          <img src={car2} alt="f1 car" className="image-container_image" />
+          <img src={car3} alt="f1 car" className="image-container_image" />
+          <img src={car4} alt="f1 car" className="image-container_image" />
+          <img src={car5} alt="f1 car" className="image-container_image" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfinityImage;
